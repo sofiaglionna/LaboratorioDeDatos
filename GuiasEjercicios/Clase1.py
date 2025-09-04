5305d937be172b727c5b6e202d61b246b5c555ba
sofiaglionna <sofiaglionna@gmail.com>
2025-09-03 22:23:48 -0300

    Update Clase1.py

--- GuiasEjercicios/Clase1.py
+++ GuiasEjercicios/Clase1.py
@@ -1,4 +1,6 @@
 import csv
+import pandas as pd
+import numpy as np
 
 
 def leer_parque(nombre_archivo,parque) -> list[dict]:
@@ -30,6 +32,16 @@ def especies(lista_arboles: list[dict]):
         listaEspecies.append(arbol['nombre_com'])
 
     return listaEspecies
+
+def especiesSinRepetidos(lista_arboles):
+    listaEspeciesSinRepetidos = []
+
+    for arbol in lista_arboles:
+        especie = arbol['nombre_com']
+        if especie not in listaEspeciesSinRepetidos:
+            listaEspeciesSinRepetidos.append(especie)
+
+    return listaEspeciesSinRepetidos
 
 #lista = leer_parque("arbolado-en-espacios-verdes.csv", "CENTENARIO")
 #print(especies(arboles))
@@ -78,6 +90,89 @@ alturas = obtener_alturas(lista, 'Jacarandá')
 #print(promedio(alturas))
 #CENTENARIO -> Más Alto: 18 mts || Promedio = 8.96 mts
 #GENERAL PAZ -> Más Alto: 16 mts || Promedio = 10.2 mts
+
+def obtener_inclinaciones(lista_arboles, especie):
+    listaInclinaciones = []
+    for arbol in lista_arboles:
+        if arbol['nombre_com'] == especie:
+            inclinacion = arbol['inclinacio']
+            listaInclinaciones.append(int(inclinacion))
+
+    return listaInclinaciones
+
+#print(obtener_inclinaciones(lista, 'Jacarandá'))
+
+def especimen_mas_inclinado(lista_arboles):
+    inclinacionMax = 0
+    especimen_mas_inclinado = 'Ninguno'
+
+    for arbol in lista_arboles:
+        inclinacion = int(arbol['inclinacio'])
+        if inclinacion > inclinacionMax:
+            inclinacionMax = inclinacion
+            especimen_mas_inclinado = arbol['nombre_com']
+
+
+    return especimen_mas_inclinado, inclinacionMax
+
+lista1 = leer_parque("arbolado-en-espacios-verdes.csv", "EJERCITO DE LOS ANDES")
+especimen, inclinacion = especimen_mas_inclinado(lista1)
+#print(f"El árbol más inclinado es {especimen}, con una inclinación de {inclinacion}°") # Sin la 'f' al principio, no lee los valores 'especimen' e 'inclinacion'
+
+def especie_promedio_mas_inclinada(lista_arboles):
+
+    especiesEnLista = especiesSinRepetidos(lista_arboles)
+    promedioMasInclinado = 0
+    especieMasInclinada = ''
+
+    for especie in especiesEnLista:
+        promedioEspecie = promedio(obtener_inclinaciones(lista_arboles,especie))
+        if promedioEspecie > promedioMasInclinado:
+            promedioMasInclinado = promedioEspecie
+            especieMasInclinada = especie
+
+    return especieMasInclinada, promedioMasInclinado
+        
+especimen, promedioInclinacion = especie_promedio_mas_inclinada(lista1)
+#print(f'La especie mas inclinada es {especimen}, con una inclinacion promedio de {inclinacion}°')
+
+
+# Cambiamos de archivo: "arbolado-publico-lineal-2017-2018.csv"
+
+########################### Armo DataFrames permanentes ############################################
+
+df_vereda = pd.read_csv("arbolado-publico-lineal-2017-2018.csv")
+df_parque = pd.read_csv("arbolado-en-espacios-verdes.csv")
+
+#Armar un DataFrame data_arboles_veredas que tenga solamente las siguiente
+#columnas: 'nombre_cientifico', 'ancho_acera', 'diametro_altura_pecho', 'altura_arbol'
+
+data_arboles_veredas = df_vereda[["nombre_cientifico", "ancho_acera", "diametro_altura_pecho", "altura_arbol"]]
+
+#Para cada dataset, armar otro seleccionando solamente las filas correspondientes
+#a las tipas (llamalos df_tipas_parques y df_tipas_veredas, respectivamente) y las
+#columnas correspondientes al diámetro a la altura del pecho y alturas.
+
+df_tipas_parques = df_parque[df_parque['nombre_com'] == 'Tipuana Tipu'].copy()
+df_tipas_veredas = df_vereda[df_vereda['nombre_cientifico'] == 'Tipuana tipu'].copy()
+
+parques = df_tipas_parques[["diametro", "altura_tot"]].copy()
+veredas = df_tipas_veredas[["diametro_altura_pecho", "altura_arbol"]].copy()
+
+# El .copy() crea una copia independiente. Si no lo pusiera, seria solo una vista del df original.
+# --> df_tipas_veredas tiene (9330 rows x 4 columns) Aparece "Tipuana tipu" 9330 veces
+
+print(df_tipas_parques)
+
+#Usar como
+#copias (usando .copy()) para poder trabajar en estos nuevos dataframes sin
+#modificar los dataframes grandes originales. Renombrar las columnas necesarias
+#para que se llamen igual en ambos dataframes.
+
+
+
+
+
 
 
 
